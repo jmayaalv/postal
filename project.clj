dfbdffbbec8ebf16119a369b72b72b4d32e91391
Juan Maya <juan.maya@kane-group.com>
2025-03-24 22:47:38 +0100

    jakarta instead of javax

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
             :distribution :repo
             :comments "Use at your own risk"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[commons-codec "1.9"]
-                 [com.sun.mail/jakarta.mail "1.6.5"]]
+  :dependencies [[commons-codec "1.18.0"]
+                 [com.sun.mail/jakarta.mail "2.0.1"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies []}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
